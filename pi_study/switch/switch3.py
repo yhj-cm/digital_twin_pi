f8bac1d252c56532a85c0a883984285f8ad0d9ea
yhj-cm <yoonhj05911@gmail.com>
2025-05-08 16:16:30 +0900

    "pi second"

--- pi_study/switch/switch3.py
+++ pi_study/switch/switch3.py
@@ -3,29 +3,69 @@ from time import sleep
 
 gpio.setmode(gpio.BCM)
 
+
 class Led:
-    
-    def _init_(self, pin, color):
+
+    def __init__(self, pin, color):
         self.pin = pin
         self.color = color
         gpio.setup(pin, gpio.OUT)
         gpio.output(pin, gpio.LOW)
-        
+
     def blink(self, count, time):
         for _ in range(count):
-            gpio.output(pin, gpio, HIGH)
+            gpio.output(self.pin, gpio.HIGH)
             sleep(time)
-            gpio.output(pin, gpio, LOW)
+            gpio.output(self.pin, gpio.LOW)
             sleep(time)
-            
+
     def ledOn(self):
         gpio.output(self.pin, gpio.HIGH)
-        
+
     def ledOff(self):
         gpio.output(self.pin, gpio.LOW)
-        
-class button:
-    
-    def _init_(self, pin, onPressed):
-        
-    
+
+
+class Button:
+
+    def __init__(self, pin, onPressed):
+        self.pin = pin
+        self.prevState = gpio.LOW
+        self.onPressed = onPressed
+        gpio.setup(self.pin, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+
+    def waitPressed(self):
+        currentState = gpio.input(self.pin)
+        if self.checkPressed(currentState):
+            self.onPressed()
+        self.prevState = currentState
+        sleep(0.05)
+
+    def checkPressed(self, currentState):
+        return currentState == gpio.HIGH and self.prevState == gpio.LOW
+
+def ledRedFunction():
+    leds[0].blink(10, 0.5)
+
+greenLedState = False
+
+def ledGreenFunction():
+    global greenLedState
+    if greenLedState:
+        leds[1].ledOff()
+    else:
+        leds[1].ledOn()
+        greenLedState = not greenLedState
+
+leds = (Led(16, "RED"),Led(21, "GREEN"))
+buttons = (Button(13, ledRedFunction()), Button(19, ledGreenFunction()))
+
+try:
+    while True:
+        for button in buttons:
+            button.waitPressed()
+
+except KeyboardInterrupt:
+    pass
+finally:
+    gpio.cleanup()
